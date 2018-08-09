19571a0099c704ad233bc679b1e9fccce677a22a
calin-beard <barburescu.calin@gmail.com>
2018-08-09 08:18:00 +0300

    Started implementing file structure for relation

--- DataHandlingBPlusTrees/FileManager.cs
+++ DataHandlingBPlusTrees/FileManager.cs
@@ -10,6 +10,7 @@ namespace DataHandlingBPlusTrees
     class FileManager
     {
         public string Path { get; set; }
+        private int Block { get; set; } = 4096;
         //public string 
 
         public FileManager(string path)
@@ -17,21 +18,39 @@ namespace DataHandlingBPlusTrees
             this.Path = path;
         }
 
+        /// <summary>
+        /// Creates file that stores a relation
+        /// </summary>
+        /// <param name="recordLength">The fixed length of the record stored within the file</param>
+        public void CreateRelation(int recordLength)
+        {
+            if (this.Block % recordLength != 0)
+            {
+                throw new Exception("The record lenght must be a divisor of the block size: " + this.Block);
+            }
+
+
+        }
+
         public void Write(string record)
         {
             //string path = Directory.GetCurrentDirectory() + @"\test";
             //Console.WriteLine("------------" + path);
-            int block = 4096;
             using (FileStream fs = new FileStream(this.Path, FileMode.OpenOrCreate))
             {
                 Byte[] info = new UTF8Encoding(true).GetBytes(record);
-                if (info.Length < block)
+                for (int i = info.Length; i < this.Block; i++)
+                {
+                    info[i] = new UTF8Encoding(true).GetBytes('\0');
+                }
+                Console.WriteLine("-------------" + info.Length);
+                if (info.Length <= this.Block)
                 {
                     fs.Write(info, 0, info.Length);
                 }
                 else
                 {
-                    throw new Exception("Record too large. Should be max 4096 bytes (chars)");
+                    throw new Exception("Record too large. Should be max " + this.Block + " bytes (chars)");
                 }
             }
         }
